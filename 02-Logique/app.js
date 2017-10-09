a1730f53f8daf301fb0dfa46e10598b721d21915
Unknown <thomaspoint@gmail.com>
2017-10-09 14:27:15 +0200

    Ex 2 fin

--- 02-Logique/app.js
+++ 02-Logique/app.js
@@ -1,18 +1,53 @@
-var lang = "fr";
+//part 1
+
+var lang = prompt('choississez votre language (en,fr,es)');
 var msg;
 
 switch (lang) {
-  case 1:
+  case "fr":
     msg = 'Bonjour tout le monde';
   break;
 
-  case 2:
+  case "en":
     msg = 'Hello world';
   break;
 
-  case 3:
+  case "es":
     msg = 'Hola Mundo';
   break;
 }
 
 alert(msg);
+
+//part 2
+
+var score = 60;
+var result;
+
+if (score >= 90) {
+  result='Vous avez le rang A';
+}
+
+if (score < 90) {
+  result='Vous avez le rang B';
+}
+
+if (score <= 50) {
+  result='Vous avez le rang C';
+ }
+
+alert(result);
+
+//part 3
+
+var word = 'légume';
+var nbr = '5';
+var line ;
+
+if (nbr > 1 ) {
+  line = word+"s";
+} else {
+  line = word;
+}
+
+alert(line);
