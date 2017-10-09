260f40600b3a22dbacede8928d20c4551893b5a9
Unknown <thomaspoint@gmail.com>
2017-10-09 12:31:01 +0200

    Fin exercice 1

---
+++ 01-Variables/app.js
@@ -0,0 +1,29 @@
+
+
+var child = 3;
+var women = 'Manon';
+var work = 'Codeur';
+var country = 'Mons';
+
+alert("Vous serez "+work+" et habiterez à "+country+" ,et mariez a "+women+" avec "+child+" enfants.")
+
+var year = new Date();
+year = year.getFullYear();
+var birthday = 1996;
+var myAge = year - birthday
+
+alert("Vous avez "+myAge+" ans!")
+
+var ageMax = 100;
+var food = "Carotte";
+var consoFood = 12;
+var maxFood = ((consoFood*365)*(ageMax-myAge));
+
+alert("Il vous reste "+maxFood+" de "+food+" avant d'atteindre l'age de "+ageMax+"!")
+
+var result = (3) * 3 + 4 / 2;
+    result = (3)* 3 + 2;
+    result = 9 + 2;
+    result = 11;
+
+alert(result);
---
+++ 02-Logique/app.js
@@ -0,0 +1,15 @@
+var lang = prompt('What langage do you want?');
+var msg ;
+
+
+if lang = 'fr' {
+  alert('Bonjour tout le monde')
+}
+
+if lang = 'es' {
+  alert('Hola, Mundo')
+}
+
+if lang = 'en' {
+  alert('Hello world')
+}
