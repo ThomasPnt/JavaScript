afbcbcc68f4b2d0e18e3524781887c54573e31c3
user <thomaspoint@gmail.com>
2017-10-12 11:42:22 +0200

    Fin exo 5

--- 05-Tableaux/app.js
+++ 05-Tableaux/app.js
@@ -5,11 +5,42 @@ console.log(Tab[0]);
 
 var tot = 0;
 
-for ( var i = 0; i < Tab.length; i=i+1){
+for ( var i = 0; i <= Tab.length; i=i+1){
   tot = tot + i ;
 }
 
 
 console.log(tot)
 
-// A finir
+//part 2
+
+var acteurs = [
+  { nom: 'Stalone',
+    classement: 'premier'},
+  { nom: 'Dwayne Jhonson',
+    classement: 'deuxième'},
+  { nom: 'Vin Diesel',
+    classement: 'troisième'} ]
+
+for ( var i = 0 ; i < acteurs.length; i++ ) {
+  acteur = acteurs[i]
+  console.log('Le '+ acteur.classement + ' est ' + acteur.nom + '!');
+}
+
+//part 3
+
+var nit = ["Mario","Luigi","Peach"]
+var add = ["Bowser"]
+
+nitadd = nit.concat(add);
+
+console.log(nitadd);
+
+//part 3 ( 2 eme methode)
+
+var dup = nit;
+nit = dup.push('Bowser')
+
+for ( var i = 0; i <= dup.length; i++){
+  console.log(dup[i]);
+}
