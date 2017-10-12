4afe8cdcbadfd6c985136bc1008d357bb174d25f
user <thomaspoint@gmail.com>
2017-10-12 16:30:42 +0200

    Fin Jour 2

--- 06-JEUX_PENDU/app.js
+++ 06-JEUX_PENDU/app.js
@@ -1,3 +1,14 @@
 var words = ["B","O","N","J","O","U","R"]
 
 var game = ["","","","","","","",""]
+
+var test = window.prompt('Donné une lettre');
+
+function guessLetter(){
+  for (var i = 0; i < words.length; i++) {
+    word = words[i];
+      if (test == word){
+        console.log(test);
+      }
+    }
+}
