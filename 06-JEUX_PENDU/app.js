6436f4d178c06115e62952ac0bfd153fce2ab71a
Unknown <thomaspoint@gmail.com>
2017-10-13 11:05:10 +0200

    Pendu impossible

--- 06-JEUX_PENDU/app.js
+++ 06-JEUX_PENDU/app.js
@@ -1,20 +1,26 @@
 var words = ["B","O","N","J","O","U","R"]
 
-var game = ["","","","","","","",""]
-
-var test = window.prompt('Donné une lettre')
+var game = ["","","","","","",""]
 
 function guessLetter(){
   for (var i = 0; i < words.length; i++) {
-    word = words[i];
-      if (test == word[i]){
-       console.log(game.push[i]);
+    //word = words[i];
+    var test = window.prompt('Donné une lettre');
+    test.toUpperCase;
+
+    for (var y = 0; y < words.length; y++) {
+      if (words[y] == test){
+       game[y]=test;
+       console.log(game);
       }
       else if(game===words){
-       console.log('Vous avez trouvé le mot');
+       alert('Vous avez trouvé le mot');
       }
-      else {
-       var test;
-      }
+    while(words== 6) {
+      console.log('Vous avez trouvé le mot');
+    }
+    }
   }
 }
+
+guessLetter();
