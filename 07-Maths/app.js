3ecdfecd54743d3ff2cd921d36ba554f681b3637
Unknown <thomaspoint@gmail.com>
2017-10-13 11:42:19 +0200

    exo 7

---
+++ 07-Maths/app.js
@@ -0,0 +1,9 @@
+var a = [7,5,-12,6,32,18,14,-2];
+var b = [-3,9,21,36,27,54,17,35];
+
+var min = Math.min([7,5,-12,6,32,18,14,-2]);
+console.log(min);
+
+for (var i = 0; i < a.length; i++) {
+  console.log(a[i]);
+}
