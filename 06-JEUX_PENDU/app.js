36c9780ef4aadb53aab2ee7703db48a430276bf2
user <thomaspoint@gmail.com>
2017-10-13 09:31:04 +0200

    jeu du pendu pas fini

--- 06-JEUX_PENDU/app.js
+++ 06-JEUX_PENDU/app.js
@@ -2,13 +2,19 @@ var words = ["B","O","N","J","O","U","R"]
 
 var game = ["","","","","","","",""]
 
-var test = window.prompt('Donné une lettre');
+var test = window.prompt('Donné une lettre')
 
 function guessLetter(){
   for (var i = 0; i < words.length; i++) {
     word = words[i];
-      if (test == word){
-        console.log(test);
+      if (test == word[i]){
+       console.log(game.push[i]);
       }
-    }
+      else if(game===words){
+       console.log('Vous avez trouvé le mot');
+      }
+      else {
+       var test;
+      }
+  }
 }
