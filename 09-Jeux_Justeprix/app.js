f49f3f90cf5ebfac54cf0e659e9af8dfbd673886
Unknown <thomaspoint@gmail.com>
2017-10-16 11:42:49 +0200

    JustePrix fini !

--- 09-Jeux_Justeprix/app.js
+++ 09-Jeux_Justeprix/app.js
@@ -4,32 +4,32 @@ var numMin = 20;
 var numMax = 80;
 
 function numAleatoire() {
-   console.log((Math.floor((Math.random()*(numMax-numMin))+numMin)));
+   return (Math.floor((Math.random()*(numMax-numMin))+numMin));
 }
 
-var win = numAleatoire();
+var result = numAleatoire()
+
+function principal() {
+  var input = window.prompt('Trouve le nombre entre 20 et 80');
+  essai++;
+  if (input == result ) {
+    victoire();
+  }
+  else if (input > result ) {
+    window.alert("C'est moins");
+    principal();
+  }
+   else if (input < result) {
+     window.alert("C'est plus");
+     principal();
+  }
+}
 
 function victoire() {
   console.log("C'est juste tu as trouvé en "+essai+" coups!");
   document.write("C'est juste tu as trouvé en "+essai+" coups!");
 }
 
-function principal() {
-  var input = window.prompt(' Trouve le nombre entre 20 et 80');
-  essai + 1;
-    if (input == win) {
-      victoire();
-    }
-    else if (input > win) {
-      window.alert("C'est moins");
-      principal();
-    }
-    else if (input < win) {
-      window.alert("C'est plus");
-      principal();
-    }
-}
 
 principal();
-
-//Comprend pas pourquoi ca marche pas
+  
