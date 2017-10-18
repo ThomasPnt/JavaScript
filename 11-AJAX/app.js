bed433aa7f846d4c559766cf3d4e061239560c32
Unknown <thomaspoint@gmail.com>
2017-10-18 12:34:21 +0200

    exo 11

--- 11-AJAX/app.js
+++ 11-AJAX/app.js
@@ -1,3 +1,10 @@
-var data = new XMLHttpRequest()
+var tip = new XMLHttpRequest()
 
-data.open('GET','https://raw.githubusercontent.com/becodeorg/Lovelace-promo-2/master/Parcours/JavaScript/11-AJAX/files/data.json', true );
+tip.open('GET','https://raw.githubusercontent.com/becodeorg/Lovelace-promo-2/master/Parcours/JavaScript/11-AJAX/files/data.json', true );
+tip.send();
+
+tip.onreadystatechange = function () {
+  if (tip.readyState === 4 ) {
+  console.log(tip.responseText);
+  }
+}
