93ca0aa682c9e91fa1930c18a2b9e7b56f1f5545
Unknown <thomaspoint@gmail.com>
2017-10-19 12:07:59 +0200

    pendu fin

--- 06-JEUX_PENDU/app.js
+++ 06-JEUX_PENDU/app.js
@@ -1,23 +1,43 @@
-var words = ["B","O","N","J","O","U","R"]
 
-var game = ["","","","","","",""]
+var mot = ['B','O','N','J','O','U','R'];
+var lettreDevinee=['','','','','','',''];
+var compteurVictoire = 0;
 
-function guessLetter(){
-  for (var i = 0; i < words.length; i++) {
-    //word = words[i];
-    var test = window.prompt('Donné une lettre');
-    test.toUpperCase;
+function guessLetter() {
+var message=prompt("Entrer un lettre à deviner");//return ce qu'envoite l'utilisateur
+message = message.toUpperCase(); // Mettre la saisie en majuscule !
+console.log(message);
+if (message.length != 1) { // On vérifie que l'utilisateur a saisi UN SEUL caractère
+console.log("Trop de caractères, vous avez écrit " + message.length + " caractères...");
+}
+else {
 
-    for (var y = 0; y < words.length; y++) {
-      if (words[y] == test){
-       game[y]=test;
-       console.log(game);
-      }
-      else if(game===words  ){
-       alert('Vous avez trouvé le mot');
-      }
+    if ( mot.indexOf(message) == -1 ) {
+        console.log("Introuvable !");
     }
-  }
+    else {
+        console.log("Trouvé !");
+
+        for (i=0;i<mot.length;i++) {
+            if (message == mot[i]) {
+                compteurVictoire++;
+                console.log("compteur " + compteurVictoire);
+                console.log("yeah ! J'ai trouvé ta lettre à l'index " + i);
+                lettreDevinee.splice(i,1,message);
+                console.log(lettreDevinee);
+
+            }
+        }
+    }
+
+}
+
+if (compteurVictoire ==  mot.length) {
+    console.log("win !");
+}
+else  {
+    guessLetter();
+}
 }
 
 guessLetter();
--- 11-AJAX/app.js
+++ 11-AJAX/app.js
@@ -1,4 +1,6 @@
 var tip = new XMLHttpRequest()
+var data = document.getElementById('result');
+
 
 tip.open('GET','https://raw.githubusercontent.com/becodeorg/Lovelace-promo-2/master/Parcours/JavaScript/11-AJAX/files/data.json', true );
 tip.send();
@@ -6,5 +8,6 @@ tip.send();
 tip.onreadystatechange = function () {
   if (tip.readyState === 4 ) {
   console.log(tip.responseText);
+  data.innerHTML = tip.responseText;
   }
 }
