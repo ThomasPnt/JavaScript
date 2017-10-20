c78d41febdecc50ba2b98d2a1eb0c56ec7eef0c2
Unknown <thomaspoint@gmail.com>
2017-10-20 11:51:45 +0200

    onaimepasajax

--- 11-AJAX/app.js
+++ 11-AJAX/app.js
@@ -1,13 +1,87 @@
-var tip = new XMLHttpRequest()
-var data = document.getElementById('result');
+// var tip = new XMLHttpRequest()
+// var data = document.getElementById('result');
+//
+//
+// tip.open('GET','https://raw.githubusercontent.com/becodeorg/Lovelace-promo-2/master/Parcours/JavaScript/11-AJAX/files/data.json', true );
+// tip.send();
+//
+// tip.onreadystatechange = function () {
+//   if (tip.readyState === 4 ) {
+//   // console.log(tip.responseText);
+//   // data.innerHTML = tip.responseText;
+//   }
+// }
 
+// var score = Math.floor(Math.random()*1000)+0;
+// console.log(score);
 
-tip.open('GET','https://raw.githubusercontent.com/becodeorg/Lovelace-promo-2/master/Parcours/JavaScript/11-AJAX/files/data.json', true );
+var tab = function () {
+  var resultat = JSON.parse(tip.responseText);
+  console.log(resultat);
+  call(resultat);
+}
+
+var tip = new XMLHttpRequest();
+tip.onload = tab;
+tip.open('GET','https://raw.githubusercontent.com/becodeorg/Lovelace-promo-2/master/Parcours/JavaScript/11-AJAX/files/data.json', true);
 tip.send();
 
-tip.onreadystatechange = function () {
-  if (tip.readyState === 4 ) {
-  console.log(tip.responseText);
-  data.innerHTML = tip.responseText;
+var result = document.getElementsByClassName('result');
+
+
+function call(resultat) {
+  // function score() {
+  //   Math.floor(Math.random()*1000)+0
+  // }
+  //
+  // score()
+  //
+  // function bahrain(country) {
+  //   return country === "Bahrain";
+  // }
+  resultat.forEach(function(user){
+  var score = Math.floor(Math.random()*1000)+0;
+  user.score = score;
+  return user;
+
+  })
+
+  resultat.sort(function(user,user1){
+  return user.score - user1.score;
+  })
+
+  var tab1 = [];
+  var tab2 = [];
+  var tab3 = [];
+
+  resultat.forEach(function(user){
+   if(user.score > 750) {
+    return tab1.push(user);
+  } else if (user.score > 500 ) {
+      return tab2.push(user);
+  } else {
+      return tab3.push(user);
   }
+  })
+
+  console.log(tab1);
+  console.log(tab2);
+  console.log(tab3);
+
+  var tab4 = [];
+
+  resultat.filter(function(user){
+    if(user.country == "Bahrain"){
+      return tab4.push(user);
+    }
+  })
+  console.log(tab4.length +' personnes vivent à Bahrain!');
+
+  resultat.sort(function(user,user1){
+    var scoreMax = user1.score - user.score;
+    return scoreMax;
+    console.log(scoreMax);
+  })
+
+
 }
